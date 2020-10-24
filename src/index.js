d49816da461eca3cec11dd614c095c21e05814e4
devat-youtuber <battuosai01@gmail.com>
2020-10-24 21:03:43 +0700

    update

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import {BrowserRouter as Router, Route} from 'react-router-dom'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Route path="/shopping-cart-react" component={App} />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
